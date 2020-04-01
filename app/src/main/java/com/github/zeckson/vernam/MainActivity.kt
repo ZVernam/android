fe66aad99a76f8a972d8be8cccd2d9c09bf22789
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-04-02 01:30:32 +0300

    fixed hints

--- app/src/main/java/com/github/zeckson/vernam/MainActivity.kt
+++ app/src/main/java/com/github/zeckson/vernam/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.inputs_layout.*
 
 
 class MainActivity : AppCompatActivity() {
