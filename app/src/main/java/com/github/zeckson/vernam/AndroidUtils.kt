c5a5b307b57700f51bd1ff52b1028663725ff097
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-04-10 00:17:25 +0300

    add encrypted settings

--- app/src/main/java/com/github/zeckson/vernam/AndroidUtils.kt
+++ app/src/main/java/com/github/zeckson/vernam/AndroidUtils.kt
@@ -1,11 +1,14 @@
 package com.github.zeckson.vernam
 
 import android.app.Activity
+import android.app.Application
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 
 fun Intent?.getHost(): String? {
     when (this?.action) {
@@ -31,5 +34,16 @@ fun Activity.setResultText(it: String?) {
 fun Activity.showToast(text: String) {
     Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }
+
+private val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+private const val PREFERENCE_KEY = "com.github.zeckson.user_secret_preferences"
+
+fun Application.getEncryptedPreferences() = EncryptedSharedPreferences.create(
+    PREFERENCE_KEY,
+    masterKeyAlias,
+    this,
+    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+)
 
 
