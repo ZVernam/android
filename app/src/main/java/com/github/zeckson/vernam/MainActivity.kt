33787f1aca6216d6acb9faf21792e2b818e8f931
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-04-10 00:17:11 +0300

    add cryptography

--- app/src/main/java/com/github/zeckson/vernam/MainActivity.kt
+++ app/src/main/java/com/github/zeckson/vernam/MainActivity.kt
@@ -2,20 +2,29 @@ package com.github.zeckson.vernam
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.inputs_layout.*
 
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MyActivity"
+    private val sharedPreferences by lazy {
+        PreferenceManager.getDefaultSharedPreferences(this)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        setupTextListeners()
 
         intent.getHost()?.let {
             plainText.setText(it)
@@ -26,6 +35,31 @@ class MainActivity : AppCompatActivity() {
             return finish()
         }
 
+    }
+
+    private fun setupTextListeners() {
+        val textWatcher = object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) = updateSecret()
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // do nothing
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // do nothing
+            }
+        }
+
+        plainText.addTextChangedListener(textWatcher)
+        passwordText.addTextChangedListener(textWatcher)
+    }
+
+    private fun updateSecret() {
+        val token = sharedPreferences.getString(getString(R.string.preference_token), "")
+        val plainText = plainText.text.toString() + token
+        val password = passwordText.text.toString()
+        val isHashed = sharedPreferences.getBoolean(getString(R.string.preference_is_hashed), false)
+        cipherText.setText(encrypt(if (isHashed) hash(plainText) else plainText, hash(password)))
     }
 
 
