58691b8235755a23e87ac647e9662e7b1416e594
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-07-31 14:22:05 +0300

    move settings.gradle.kts to Kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+rootProject.buildFileName = "build.gradle.kts"
+rootProject.name = "Vernam"
+include(":app")
+
+gradle.beforeProject {
+    val localPropertiesFile = rootDir.resolve("local.properties")
+    if (localPropertiesFile.exists()) {
+        val localProperties = java.util.Properties()
+        localProperties.load(localPropertiesFile.inputStream())
+        localProperties.forEach { (k, v) -> if (k is String) project.extra.set(k, v) }
+    }
+}
