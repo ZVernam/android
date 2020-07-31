2d30a8baac2a5e86627e59af37e5e8bf030e2a76
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-07-31 14:24:07 +0300

    convert build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
 
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build.gradle.kts files
     }
 }
 
@@ -28,6 +28,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+task.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
