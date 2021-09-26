156dec97ff4cd3d6e91be3c4c1f912af65aaea84
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2021-09-26 14:01:43 +0300

    code cleanup

--- app/src/main/java/com/github/zeckson/vernam/SettingsWrapper.kt
+++ app/src/main/java/com/github/zeckson/vernam/SettingsWrapper.kt
@@ -27,8 +27,7 @@ class SettingsWrapper private constructor(
         val passwordIV = getPasswordIV() ?: return PasswordState.NOT_SET
 
         // Password set, but biometric was disabled or gone(((
-        if (BiometricManager.from(context)
-                .canAuthenticate() != BiometricManager.BIOMETRIC_SUCCESS
+        if (context.biometricStatus != BiometricManager.BIOMETRIC_SUCCESS
         ) return PasswordState.RESET
 
         // Password is set, but invalidated (due biometrics param change)
@@ -44,19 +43,13 @@ class SettingsWrapper private constructor(
     private fun getEncodedPassword() =
         preferences.getString(getString(R.string.preference_password), null)
 
-    private fun getString(resId: Int): String? {
+    private fun getString(resId: Int): String {
         return context.getString(resId)
     }
 
     val isHashed: Boolean
         get() = preferences.getBoolean(getString(R.string.preference_is_hashed), true)
 
-    val isBiometricEnabled: Boolean
-        get() {
-            val password = getEncodedPassword()
-            return !password.isNullOrEmpty() && BiometricManager.from(context)
-                .canAuthenticate() == BiometricManager.BIOMETRIC_SUCCESS
-        }
 
     private fun getEncodedPasswordAndIv(): Pair<ByteArray, ByteArray>? {
         val password = getEncodedPassword()
@@ -81,15 +74,11 @@ class SettingsWrapper private constructor(
         return decoded.toString(Charsets.UTF_8)
     }
 
-    fun clearPassword() {
-        preferences.edit().putString(getString(R.string.preference_password), "").apply()
-    }
 
     val suffix: String
         get() = preferences.getString(getString(R.string.preference_suffix), EMPTY_STRING)!!
 
     companion object {
-        private const val MAX_CIPHER_SIZE_DEFAULT = 15
         private const val EMPTY_STRING = ""
         fun get(ctx: Context): SettingsWrapper {
             return SettingsWrapper(PreferenceManager.getDefaultSharedPreferences(ctx), ctx)
--- app/src/main/java/com/github/zeckson/vernam/UrlUtils.kt
+++ app/src/main/java/com/github/zeckson/vernam/UrlUtils.kt
@@ -5,7 +5,7 @@ import java.util.*
 private const val WWW_PREFIX = "www."
 
 fun stripUrlToHost(url: String, stripWWW: Boolean = true): String {
-    var result = url.toLowerCase(Locale.ENGLISH)
+    var result = url.lowercase(Locale.ENGLISH)
     // remove invalid scheme like "fun////"
     val scheme = result.lastIndexOf("//")
     if (scheme >= 0) {
